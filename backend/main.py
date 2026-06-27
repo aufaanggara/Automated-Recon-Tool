c97dcb2ea7a0f21d8d78cdf23201e3c993317e99
aufaanggara <mfaufalaha@gmail.com>
2026-06-27 05:33:25 +0000

    black formatter work

--- backend/main.py
+++ backend/main.py
@@ -4,26 +4,33 @@ import subprocess
 
 app = FastAPI()
 
+
 class ScanRequest(BaseModel):
     domain: str
-    
+
+
 @app.get("/")
 def local():
-    return {"status" : "on"}
+    return {"status": "on"}
+
 
 @app.post("/scan")
 def scan_domain(request: ScanRequest):
     result = subprocess.run(
         ["/home/aufa/go/bin/subfinder", "-d", request.domain, "-timeout", "30"],
         capture_output=True,
-        text=True
+        text=True,
     )
     subdomains = [s for s in result.stdout.split("\n") if s]
     httpx_result = subprocess.run(
         ["/home/aufa/go/bin/httpx", "-silent"],
         input="\n".join(subdomains),
         capture_output=True,
-        text=True
+        text=True,
     )
     live_hosts = [s for s in httpx_result.stdout.split("\n") if s]
-    return {"domain": request.domain, "subdomains": subdomains, "live hosts": live_hosts}
+    return {
+        "domain": request.domain,
+        "subdomains": subdomains,
+        "live hosts": live_hosts,
+    }
